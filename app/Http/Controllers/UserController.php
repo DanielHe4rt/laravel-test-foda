7d630ebda9b4e85fa42e57b8e68fde617888aa2c
danielhe4rt <hey@danielheart.dev>
2023-12-26 04:06:18 -0300

    feat: adding users related code

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+
+class UserController extends Controller
+{
+    public function viewUserList(): View
+    {
+        return view('');
+    }
+}
